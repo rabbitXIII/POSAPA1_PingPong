b46886651005829d9f5faced028ae71a593a831c
Rohit Gopal <rohitgopal@gmail.com>
2013-08-18 14:48:40 -0400

    Prints MAX_NUMBER hits, but does not coordinate.
    The first thread gets a head start and hits the ball many times in a
    row, since there is no waiting for the other thread yet.

--- src/PingPongManager.java
+++ src/PingPongManager.java
@@ -1,64 +1,71 @@
 import java.util.concurrent.CountDownLatch;
 
-
-
 public class PingPongManager {
-	
-	private final int MAX_HITS_IN_GAME = 10;
-	
+
+	private static final int MAX_HITS_IN_GAME = 10;
+
 	private Thread[] threads = new Thread[2];
-	
-    private CountDownLatch startSignal = new CountDownLatch(1);
-    
-    private CountDownLatch totalHits = new CountDownLatch(MAX_HITS_IN_GAME);
-	
-	
+
+	private static CountDownLatch totalHits = new CountDownLatch(MAX_HITS_IN_GAME);
+
 	public static void main(String[] args) {
 
-		PingPongManager game = new PingPongManager();
+		PingPongManager manager = new PingPongManager();
+		
+		manager.startGame();
+		manager.waitAndFinishGame();
 
 	}
-	
-	public PingPongManager(){
+
+	public PingPongManager() {
 		threads[0] = new Thread(new PingPongPlayer("Ping!"));
-		threads[1] = new Thread(new PingPongPlayer("Pong!"));	
+		threads[1] = new Thread(new PingPongPlayer("Pong!"));
 	}
-	
-	public void startGame(){
-		System.out.println("Ready… Set… Go!\n");
-		for (Thread thread: threads)
-			thread.start();
-		
-	}
-	
-	public void waitForGame() throws InterruptedException{
-		for (Thread thread : threads) {
-			thread.join();
 
+	public void startGame() {
+		System.out.println("Ready… Set… Go!\n");
+		for (Thread thread : threads)
+			thread.start();
+
+	}
+
+	public void waitAndFinishGame(){
+		try {
+			for (Thread thread : threads) {
+				thread.join();
+
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-		
+
 		System.out.println("\nDone!");
 	}
-	
+
 	public class PingPongPlayer implements Runnable {
-		
+
 		private String sound;
-		private CountDownLatch startSignal;
-		
-		public PingPongPlayer(String sound){
+
+		public PingPongPlayer(String sound) {
 			this.sound = sound;
+		}
+
+		private boolean moreHitsInGame() {
+			return totalHits.getCount() > 0;
 		}
 		
 		@Override
-		public void run(){
-			hitAndPrint();
+		public void run() {
+			while(moreHitsInGame()){
+				hitAndPrint();
+			}
 		}
 
 		private synchronized void hitAndPrint() {
+			totalHits.countDown();
 			System.out.println(sound);
 		}
 	}
 
 }
-
